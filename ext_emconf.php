a9c1784470c757fb40c970ca721ef9a7da82acd9
Andreas Kiessling <andreas@powered-by-coffee.de>
2018-12-06 00:20:07 +0100

    set TYPO3 and form constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '8.7.0-9.5.99',
+            'form' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
